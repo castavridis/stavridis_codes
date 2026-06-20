7cffe50193e615fe3bd1ab988b9f3b5b8f1e938d
C Stavridis <castavridis@gmail.com>
2026-06-20 05:23:02 -0500

    [Draft] Starting to input content

---
+++ content/projects/CareSignalDSComponentsV2.tsx
@@ -0,0 +1,12 @@
+import {
+	MDXWrapper,
+	MDXColumn,
+} from '../../src/components/mdx/Layout';
+
+export function Callout () {
+	return (
+		<MDXWrapper>
+			<img src="/images/caresignal-ds/v2/Task-Based Dashboard.png" className="rounded-lg" />
+		</MDXWrapper>
+	)
+}
